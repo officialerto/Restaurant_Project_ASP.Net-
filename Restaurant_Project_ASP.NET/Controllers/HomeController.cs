d934379466c9dc4d0b7bf013d48d54dcfad70cf1
Ertugrul <57365047+officialerto@users.noreply.github.com>
2023-01-23 22:09:12 +0300

    Last_Update_24.01.2023_23.31

--- Restaurant_Project_ASP.NET/Controllers/HomeController.cs
+++ Restaurant_Project_ASP.NET/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Restaurant_Project_ASP.NET.Repositories;
+﻿using Restaurant_Project_ASP.NET.Models;
+using Restaurant_Project_ASP.NET.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,12 @@ namespace Restaurant_Project_ASP.NET.Controllers
 {
     public class HomeController : Controller
     {
+        private RestaurantDBEntities objRestaurantDbEntities;
+        public HomeController()
+        {
+            objRestaurantDbEntities = new RestaurantDBEntities();
+        }
+
         // GET: Home
         public ActionResult Index()
         {
@@ -19,6 +26,14 @@ namespace Restaurant_Project_ASP.NET.Controllers
             var objMultipleModels = new Tuple<IEnumerable<SelectListItem>, IEnumerable<SelectListItem>, IEnumerable<SelectListItem>>
                 (objCustomerRepository.GetAllCustomer(), objItemRepository.GetAllItems(), objPaymentTypeRepository.GetAllPaymentType());
             return View(objMultipleModels);
+        }
+
+        [HttpGet]
+        public JsonResult getItemUnitPrice(int itemId)
+        {
+            decimal UnitPrice = objRestaurantDbEntities.Items.Single(model => model.ItemId == itemId).ItemPrice;
+
+            return Json(UnitPrice, JsonRequestBehavior.AllowGet);
         }
     }
 }
